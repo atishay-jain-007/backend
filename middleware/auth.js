a3c4795cb8a8877215104177dd8a7579ee43d1b8
atishay jkain <atishay.jain@ssipmt.com>
2024-05-23 07:32:25 +0530

    cookies

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-// middleware/auth.js
+
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
@@ -8,16 +8,16 @@ const verifyToken = (req, res, next) => {
         return res.status(403).json({ error: 'Token not provided' });
     }
 console.log(token)
-    // Verify token
+
     jwt.verify(token, 'jsooon', (err, decoded) => {
         if (err) {
             console.error('Error verifying token:', err);
             return res.status(401).json({ error: 'Failed to authenticate token' });
         }
 
-        // Attach user information to request for further processing
+       
         req.user = decoded;
-        next(); // Pass control to the next middleware
+        next(); 
     });
 };
 
